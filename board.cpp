a8198ac4f5869a99180dac555c1acf15028a954c
ASUS <shahryarazad1234@gmail.com>
2024-01-01 22:20:59 +0330

    added movement system...bfs and wall check missing

--- board.cpp
+++ board.cpp
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <wchar.h>
+//data storage
+struct player{
+    int id;
+    char name[20];
+    int x ,y;
+    int wallNumber;
+
+};
+
 int placeWall(int board[][50] , int wallx ,int wally , char rotation){
     if(rotation == 'H'){
         for (int i = 0; i < 5; ++i) {
@@ -14,6 +23,45 @@ int placeWall(int board[][50] , int wallx ,int wally , char rotation){
 
 
     return 0;
+}
+void move(int board[][50],player &player , char direction){
+    if(direction == 'w'){
+        board[2*(player.y) +1][2*(player.x) +1] = 2;
+        player.y--;
+        if(player.id == 1)
+            board[2*(player.y) +1][2*(player.x) +1] = 3;
+        else
+            board[2*(player.y) +1][2*(player.x) +1] = 4;
+    }
+
+    else if(direction == 'a'){
+        board[2*(player.y) +1][2*(player.x) +1] = 2;
+        player.x--;
+        if(player.id == 1)
+            board[2*(player.y) +1][2*(player.x) +1] = 3;
+        else
+            board[2*(player.y) +1][2*(player.x) +1] = 4;
+    }
+
+    else if(direction == 'd'){
+        board[2*(player.y) +1][2*(player.x) +1] = 2;
+        player.x++;
+        if(player.id == 1)
+            board[2*(player.y) +1][2*(player.x) +1] = 3;
+        else
+            board[2*(player.y) +1][2*(player.x) +1] = 4;
+    }
+
+    else if(direction == 's'){
+        board[2*(player.y) +1][2*(player.x) +1] = 2;
+        player.y++;
+        if(player.id == 1)
+        board[2*(player.y) +1][2*(player.x) +1] = 3;
+        else
+            board[2*(player.y) +1][2*(player.x) +1] = 4;
+    }
+
+
 }
 int intprintBoard(int arr[][50], int n) {
     for (int i = 0; i < n; ++i) {
@@ -53,7 +101,7 @@ int main(void) {
     int n;
     printf("dimention of board:");
     scanf("%d", &n);
-    int board[50][50] = {{0}}; // board[2n +1][2n+1]
+    int board[50][50] = {{0}};
     for (int i = 0; i < 2 * n + 1; ++i) {
         for (int j = 0; j < 2 * n + 1; ++j) {
             if (i == 0 || i == 2 * n || j == 0 || j == 2 * n) { board[i][j] = 1; }
@@ -65,42 +113,84 @@ int main(void) {
         }
     }
 
+
+
+    //establish parameters
+    player p1 , p2;
+    p1.id =1 , p2.id = 2;
+    p1.x = (n-1)/2 , p1.y = 0 , p2.x = (n-1)/2 , p2.y = n-1;
+    printf("give me how many walls each player can place");
+    scanf("%d %d" , &p1.wallNumber , &p2.wallNumber);
+    printf("p1...what is your name?");
+    scanf("%19s" , p1.name);
+    printf("p2...what is your name?");
+    scanf("%19s" , p2.name);
 //places players in position
-    int player1[2], player2[2];
-    printf("position of p1?");
-    scanf("%d %d", &player1[0], &player1[1]);
-    printf("position of p2?");
-    scanf("%d %d", &player2[0], &player2[1]);
-    board[player1[0] * 2 + 1][player1[1] * 2 + 1] = 3;
-    board[player2[0] * 2 + 1][player2[1] * 2 + 1] = 4;
-
-//place walls
-int p1wall , p2wall;
-    printf("how many wall does p1 want to place?");
-    scanf("%d" , &p1wall);
-    for (int i = 0; i < p1wall; ++i) {
-        int wallx , wally;
-        char rotation;
-        printf("where do you want your wall?");
-        scanf("%d %d %c", &wallx , &wally , &rotation);
-        placeWall(board ,wallx,wally,rotation);
+    board[2*(p1.y) +1][2*(p1.x) +1] = 3;
+    board[2*(p2.y) +1][2*(p2.x) +1] = 4;
+//move system
+while(p2.y != 0 && p1.y != n-1){
+    //player 1 does something
+    int moveID;
+    do{
+        printf("what do you wanna do %s?\n 1:move   2:place wall" , p1.name);
+        scanf("%d" , &moveID);
+        if(moveID == 1)
+            break;
+    }while(p1.wallNumber == 0);
+    if(moveID == 1){
+        char dir;
+        printf("use WASD to move");
+        scanf(" %c" , &dir);
+        move(board , p1, dir);
+    }else{
+        int x , y ;
+        char r;
+        printf("give me the location and rotation");
+        scanf("%d %d %c" , &x , &y , &r);
+        placeWall(board,x , y , r);
+        p1.wallNumber--;
     }
-    printf("\n");
-    printf("how many wall does p2 want to place?");
-    scanf("%d" , &p2wall);
-    for (int i = 0; i < p2wall; ++i) {
-        int wallx , wally;
-        char rotation;
-        printf("where do you want your wall?");
-        scanf("%d %d %c", &wallx , &wally , &rotation);
-        placeWall(board ,wallx,wally,rotation);
+    printBoard(board, 2 * n + 1);
+
+
+    //player 2 does something
+
+
+    do{
+        printf("what do you wanna do %s?\n 1:move   2:place wall" , p2.name);
+        scanf("%d" , &moveID);
+        if(moveID == 1)
+            break;
+    }while(p2.wallNumber == 0);
+    if(moveID == 1){
+        char dir;
+        printf("use WASD to move");
+        scanf(" %c" , &dir);
+        move(board,p2, dir);
+    }else{
+        int x , y ;
+        char r;
+        printf("give me the location and rotation");
+        scanf("%d %d %c" , &x , &y , &r);
+        placeWall(board,x , y , r);
+        p2.wallNumber--;
     }
-//test print board
-    printf("\n");
-//intprintBoard(board, 2 * n + 1);
+    printBoard(board, 2 * n + 1);
 
 
-//print board
+
+
+}
+
+
+
+
+
+
+
+
+
 
 printBoard(board, 2 * n + 1);
 
